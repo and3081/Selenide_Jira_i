0b6c99572291dde3d6b37ca2e0dc8a5dacffea43
awa <and3081@yandex.ru>
2022-04-28 13:38:13 +0300

    10

--- src/test/java/PageSteps/BaseSteps.java
+++ src/test/java/PageSteps/BaseSteps.java
@@ -1,14 +1,5 @@
 package PageSteps;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.visible;
-
 public class BaseSteps {
-    public static void clicktoSmth(){
-//        inputPath.shouldBe(visible, Duration.ofSeconds(60)).click();
-//        inputPath.setValue("Marvel");
-//        findButton.click();
-    }
 
 }
